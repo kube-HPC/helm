8dd0de51d4fd90ae25c921c09f4ca83fc6a821fa
Eli Maor <eli.maor@gmail.com>
2018-07-19 16:17:42 +0300

    fix json isue

--- load-version/index.js
+++ load-version/index.js
@@ -3,11 +3,9 @@ var request = require('request');
 request('https://github.com/kube-HPC/release-manager/releases/download/1.1.291/version.json', function (error, response, body) {
   if (!error && response.statusCode == 200) {
      var importedJSON = JSON.parse(body);
-     var json = JSON.parse(body);
-     alert(json.systemVersion);
-     alert(json.versions.project);
-     alert(json.versions.tag); 
-     console.log(importedJSON);
+     console.log(importedJSON.systemVersion);
+     console.log(importedJSON.versions[0].project);
+     console.log(importedJSON.versions);
+     //console.log(importedJSON);
   }
 })
-
