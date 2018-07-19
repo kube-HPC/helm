45fb6cf27040ec86327962a938eda9d9a8e5e556
Eli Maor <eli.maor@gmail.com>
2018-07-19 16:59:55 +0300

    jeson isue

--- load-version/index.js
+++ load-version/index.js
@@ -3,9 +3,17 @@ var request = require('request');
 request('https://github.com/kube-HPC/release-manager/releases/download/1.1.291/version.json', function (error, response, body) {
   if (!error && response.statusCode == 200) {
      var importedJSON = JSON.parse(body);
+     importedJSON.forEach((o=>console.log(o))) 
      console.log(importedJSON.systemVersion);
      console.log(importedJSON.versions[0].project);
-     console.log(importedJSON.versions);
+      //console.log(importedJSON.versions);
      //console.log(importedJSON);
   }
 })
+
+
+// var a = [{a:1,b:{c:"ass"}},{a:2,b:{c:"adasda"}}]
+// undefined
+// > a.forEach((d=>console.log(d.b.c)))
+// ass
+// adasda
