a5b42607e2e612cb888ab1fc60472abaf932e364
Eli Maor <eli.maor@gmail.com>
2018-07-19 14:48:32 +0300

    json read

--- load-version/index.js
+++ load-version/index.js
@@ -1,7 +1,12 @@
+var http = require('http');
 var request = require('request');
 request('https://github.com/kube-HPC/release-manager/releases/download/1.1.291/version.json', function (error, response, body) {
   if (!error && response.statusCode == 200) {
      var importedJSON = JSON.parse(body);
+     var json = JSON.parse(body);
+     alert(json.systemVersion);
+     alert(json.versions.project);
+     alert(json.versions.tag); 
      console.log(importedJSON);
   }
 })
