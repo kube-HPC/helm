9fae634fb8966dcb6503580464755efa13482408
Eli Maor <eli.maor@gmail.com>
2018-07-22 09:26:46 +0300

    add load version

--- load-version/i.js
+++
@@ -1,9 +0,0 @@
-getJSONData(url,callback);
-//callback function 
-fucntion callback(err, data) {
-  if (err) {
-    console.log('request failed');
-    return;
-  }
-  // do something with data
-}
--- load-version/index.js
+++ load-version/index.js
@@ -1,22 +1,32 @@
-var http = require('http');
+// var http = require('http');
+var fs = require('fs');
 var request = require('request');
 request('https://github.com/kube-HPC/release-manager/releases/download/1.1.291/version.json', function (error, response, body) {
   if (!error && response.statusCode == 200) {
      var importedJSON = JSON.parse(body);
-     console.log(importedJSON.systemVersion);
-     console.log(importedJSON.versions[0].project);
-
-     importedJSON.versions.forEach(obj {
-       console.log(obj)
-     }); 
-      //console.log(importedJSON.versions);
-     //console.log(importedJSON);
+     var filename = "version.json"
+     var newfilename = "versionew.json"
+     importedJSON.versions.forEach(o=>console.log("project:", o.project,"tag:",o.tag));
+     var data1 =  importedJSON.versions.forEach(o=>console.log("project:", o.project,"tag:",o.tag));
+     var data = JSON.stringify(body);
+     fs.writeFileSync(filename, data);
+     fs.writeFileSync(newfilename, data1);
   }
 })
 
+// var filename = '/tmp/crap1';
 
-// var a = [{a:1,b:{c:"ass"}},{a:2,b:{c:"adasda"}}]
-// undefined
-// > a.forEach((d=>console.log(d.b.c)))
-// ass
-// adasda
+// fs.readFile(filename, function(err, data) {
+//     if(err) throw err;
+//     theFile = data.toString().split("\n");
+//     theFile.splice(-3,3);
+//     fs.writeFile(filename, theFile.join("\n"), function(err) 
+
+     //console.log(importedJSON.systemVersion);
+     //console.log(importedJSON.versions[0].project);
+     //importedJSON.versions.forEach(o=>console.log(o.tag));
+     //console.log(importedJSON.versions.forEach(o=>console.log(o)));
+     //console.log(importedJSON.versions);
+     //console.log(importedJSON);
+
+
