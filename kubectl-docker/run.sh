824b7bb74870545bb37a83fc2de881f3777100eb
Eli Maor <eli.maor@gmail.com>
2018-07-23 11:09:49 +0300

    move to new proj

--- kubectl-docker/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-ALLDEPLOYS=$(kubectl get deploy -l metrics-group=algorithm-queue  | grep algorithm-queue | awk '{ print $1 }')
-ALLJOBS=$(kubectl get jobs | grep clean-old-jobs | awk '{ print $1 }')
-echo ALLJOBS=$ALLJOBS
-echo ALLDEPLYS=$ALLDEPLOYS
-kubectl delete deploy -l metrics-group=algorithm-queue 
-kubectl delete job -l delete-group=delete-dep-and-jobs
--- load-version/getver.js
+++
@@ -1,9 +0,0 @@
-var request = require('request');
-var url = 'https://github.com/kube-HPC/release-manager/releases/download/1.1.291/version.json';
-var jsonObject;
-request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-     jsonObject = JSON.parse(body);
-     console.log(jsonObject);
-  }
-});
