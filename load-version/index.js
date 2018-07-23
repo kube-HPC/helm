fa97b670cdca0d838ad5d1e4758c32ebce202749
Eli Maor <eli.maor@gmail.com>
2018-07-23 09:59:23 +0300

    add version.json

--- load-version/index.js
+++ load-version/index.js
@@ -1,32 +1,44 @@
 // var http = require('http');
 var fs = require('fs');
 var request = require('request');
-request('https://github.com/kube-HPC/release-manager/releases/download/1.1.291/version.json', function (error, response, body) {
+yaml = require('js-yaml');
+request('https://github.com/kube-HPC/release-manager/releases/download/1.1.292/version.json', function (error, response, body) {
   if (!error && response.statusCode == 200) {
      var importedJSON = JSON.parse(body);
      var filename = "version.json"
-     var newfilename = "versionew.json"
+     const project = importedJSON.versions.forEach(o=>console.log("project:", o.project));
      importedJSON.versions.forEach(o=>console.log("project:", o.project,"tag:",o.tag));
-     var data1 =  importedJSON.versions.forEach(o=>console.log("project:", o.project,"tag:",o.tag));
-     var data = JSON.stringify(body);
-     fs.writeFileSync(filename, data);
-     fs.writeFileSync(newfilename, data1);
+     var data = JSON.stringify(body.replace(/\r?\n|\r/g, " "));
+     fs.writeFileSync(filename, data.trim());
+     var importedYAML = yaml.safeLoad(fs.readFileSync('/home/eli/Dropbox/hkube/helm/hkube/values.yaml', 'utf8'));
+     console.log(importedYAML);
+     let yamlClone = { ...importedYAML};
+     console.log(yamlClone);
+     const indentedJson = JSON.stringify(importedYAML, null,4);
+     console.log(indentedJson);
   }
 })
 
-// var filename = '/tmp/crap1';
+     //let jsonClone = { ...importedJSON };
+     //console.log(jsonClone);
+//console.log(data.replace(/\r?\n|\r/g, " "));
+//let yamlClone = { ...importedYAML };
+//let overidyamlClone = { ...myyaml.importedYAML.replace(/\r?valuetag/g, "newvaluetag") };
+//var data1 =  importedJSON.versions.forEach(o=>console.log("project:", o.project,"tag:",o.tag));
 
-// fs.readFile(filename, function(err, data) {
-//     if(err) throw err;
-//     theFile = data.toString().split("\n");
-//     theFile.splice(-3,3);
-//     fs.writeFile(filename, theFile.join("\n"), function(err) 
+// function findAndReplace(object, value, replacevalue) {
+//   for (var x in object) {
+//     if (object.hasOwnProperty(x)) {
+//       if (typeof object[x] == 'object') {
+//         findAndReplace(object[x], value, replacevalue);
+//       }
+//       if (object[x] == value) { 
+//         object["name"] = replacevalue;
+//         // break; // uncomment to stop after first replacement
+//       }
+//     }
+//   }
+// }
 
-     //console.log(importedJSON.systemVersion);
-     //console.log(importedJSON.versions[0].project);
-     //importedJSON.versions.forEach(o=>console.log(o.tag));
-     //console.log(importedJSON.versions.forEach(o=>console.log(o)));
-     //console.log(importedJSON.versions);
-     //console.log(importedJSON);
 
 
