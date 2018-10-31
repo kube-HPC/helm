b343c717687c61621c479b19b76bbfa839bafddf
Yehiyam Livneh <yehiyam@gmail.com>
2018-10-31 15:35:19 +0200

    update script

--- package.sh
+++ package.sh
@@ -1,1 +1,6 @@
-#!/bin/env bash
+#!/usr/bin/env bash
+CHART_VERSION=$(git describe --abbrev=0)
+APP_VERSION=0.0.1
+mkdir -p /tmp/helm-charts
+helm package --app-version=${APP_VERSION} --version=${CHART_VERSION} -d /tmp/helm-charts hkube
+helm package --app-version=${APP_VERSION} --version=${CHART_VERSION} -d /tmp/helm-charts thirdparty
