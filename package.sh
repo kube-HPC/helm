34f6e419d73de857bf29be986f802b44a5170403
Yehiyam Livneh <yehiyam@gmail.com>
2018-10-31 15:52:03 +0200

    script

--- package.sh
+++ package.sh
@@ -4,3 +4,8 @@ APP_VERSION=0.0.1
 mkdir -p /tmp/helm-charts
 helm package --app-version=${APP_VERSION} --version=${CHART_VERSION} -d /tmp/helm-charts hkube
 helm package --app-version=${APP_VERSION} --version=${CHART_VERSION} -d /tmp/helm-charts thirdparty
+git checkout gh-pages
+cp /tmp/helm-charts/hkube-v${CHART_VERSION}.tgz .
+cp /tmp/helm-charts/thirdparty-v${CHART_VERSION}.tgz .
+helm repo index --merge ./index.yaml /tmp/helm-charts/
+cp /tmp/helm-charts/index.yaml .
