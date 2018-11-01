75abd197356dbcb63ce0f6fd0513075a8ca06370
Yehiyam Livneh <yehiyam@gmail.com>
2018-11-01 14:28:00 +0200

    fix

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ APP_VERSION=$(grep systemversion hkube/values.yaml | awk -F': ' '{print $2}')
 mkdir -p /tmp/helm-charts
 helm package --app-version=${APP_VERSION} --version=${APP_VERSION} -d /tmp/helm-charts hkube
 git checkout gh-pages
-cp /tmp/helm-charts/hkube-${CHART_VERSION}.tgz .
+cp /tmp/helm-charts/hkube-${APP_VERSION}.tgz .
 helm repo index --merge ./index.yaml /tmp/helm-charts/
 cp /tmp/helm-charts/index.yaml .
 git add .
