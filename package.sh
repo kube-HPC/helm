40553bcd8ee7ad542aed10f29ddd9060dcde2a7a
Yehiyam Livneh <yehiyam@gmail.com>
2018-11-18 14:36:03 +0200

    try cd

--- package.sh
+++ package.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 CHART_VERSION=$(git describe --abbrev=0)
 VALUES_YAML_PATH=hkube/values.yaml NEW_VALUES_YAML_PATH=hkube/values.yaml ./version-updater
-
 APP_VERSION=$(grep systemversion hkube/values.yaml | awk -F': ' '{print $2}')
 mkdir -p /tmp/helm-charts
 helm package --app-version=${APP_VERSION} --version=${APP_VERSION} -d /tmp/helm-charts hkube
-git config  remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-git fetch origin
+git stash
 git checkout --track origin/gh-pages
 cp /tmp/helm-charts/hkube-${APP_VERSION}.tgz .
 helm repo index --merge ./index.yaml /tmp/helm-charts/
