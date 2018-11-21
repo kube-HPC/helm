3df751bd3301a2d9082f672da873aa2675d06d31
Yehiyam Livneh <yehiyam@gmail.com>
2018-11-21 13:14:49 +0200

    update script

--- package-mini-thirdparty.sh
+++ package-mini-thirdparty.sh
@@ -3,7 +3,7 @@ mkdir -p /tmp/helm-charts
 rm /tmp/helm-charts/*
 helm package -d /tmp/helm-charts mini-thirdparty
 git stash
-git checkout --track origin/gh-pages
+git checkout gh-pages
 cp /tmp/helm-charts/mini-thirdparty-*.tgz .
 helm repo index --merge ./index.yaml /tmp/helm-charts/
 cp /tmp/helm-charts/index.yaml .
