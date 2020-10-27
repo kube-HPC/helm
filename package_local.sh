36bd96fa459c7322a4a703b74944d2aeaecd95d2
Yehiyam Livneh <yehiyam@gmail.com>
2020-10-27 15:52:07 +0200

    update script

--- package_local.sh
+++ package_local.sh
@@ -8,17 +8,10 @@ rm -rf /tmp/helm-charts/*
 helm dependency update hkube
 helm package --app-version=${APP_VERSION} --version=${APP_VERSION} -d /tmp/helm-charts hkube
 git stash
-git checkout --track origin/gh-pages
+git checkout gh-pages
 cp /tmp/helm-charts/hkube-${APP_VERSION}.tgz ./
 helm repo index --merge ./index.yaml /tmp/helm-charts/
 cp /tmp/helm-charts/index.yaml ./
 git add .
 git commit -m "update charts"
-git push
-git checkout master
-git stash apply
-git add hkube/values.yaml
-git commit -m "update version to ${APP_VERSION} [skip ci]"
-git pull --no-commit
-git commit -m "merge from master [skip ci]"
 git push
