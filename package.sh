e6d9bac952758ecd8dfe8fdfb3c49ad714912121
Yehiyam Livneh <yehiyam@gmail.com>
2021-04-01 12:19:30 +0300

    increment version on each build add version to trigger payload

--- package.sh
+++ package.sh
@@ -6,12 +6,12 @@ VALUES_YAML_PATH=hkube/values.yaml NEW_VALUES_YAML_PATH=hkube/values.yaml ./vers
 APP_VERSION=$(grep systemversion hkube/values.yaml | awk -F': ' '{print $2}')
 mkdir -p /tmp/helm-charts
 helm dependency update hkube
-helm package --app-version=${APP_VERSION} --version=${APP_VERSION} -d /tmp/helm-charts hkube
+helm package --app-version=${APP_VERSION} --version=${CHART_VERSION} -d /tmp/helm-charts hkube
 git stash
 git remote -v
 git fetch
 git checkout -b gh-pages origin/gh-pages
-cp /tmp/helm-charts/hkube-${APP_VERSION}.tgz ./dev
+cp /tmp/helm-charts/hkube-${CHART_VERSION}.tgz ./dev
 helm repo index --merge ./dev/index.yaml /tmp/helm-charts/
 cp /tmp/helm-charts/index.yaml ./dev
 git add .
@@ -21,8 +21,8 @@ git checkout master
 git stash apply
 echo git add hkube/values.yaml
 git add hkube/values.yaml
-echo git commit -m "update version to ${APP_VERSION} [skip ci]"
-git commit -m "update version to ${APP_VERSION} [skip ci]"
+echo git commit -m "update version to ${CHART_VERSION} [skip ci]"
+git commit -m "update version to ${CHART_VERSION} [skip ci]"
 git pull --no-commit
 git commit -m "merge from master [skip ci]"
 git push
