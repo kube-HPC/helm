0493639ff58113d7f6a542f022a192cf7a0aa562
Yehiyam Livneh <yehiyam@gmail.com>
2021-04-28 15:33:11 +0300

    fix script

--- package-release.sh
+++ package-release.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -o xtrace
+CHART_VERSION=$(git describe --abbrev=0)
 APP_VERSION=$(grep systemversion hkube/values.yaml | awk -F': ' '{print $2}')
 mkdir -p /tmp/helm-charts
 rm /tmp/helm-charts/*
-helm package --app-version=${APP_VERSION} --version=${APP_VERSION} -d /tmp/helm-charts hkube
+helm package --app-version=${APP_VERSION} --version=${CHART_VERSION} -d /tmp/helm-charts hkube
 git checkout gh-pages
 git pull
-cp /tmp/helm-charts/hkube-${APP_VERSION}.tgz .
+cp /tmp/helm-charts/hkube-${CHART_VERSION}.tgz .
 helm repo index --merge ./index.yaml /tmp/helm-charts/
 cp /tmp/helm-charts/index.yaml .
 git add .
