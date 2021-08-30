fb95f66460aad887adc4de4b285ba5a9f1bfccd1
Yehiyam Livneh <yehiyam@gmail.com>
2021-08-30 16:44:21 +0300

    update version updated

--- package.sh
+++ package.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
-set -o xtrace
-helm lint hkube/
+set -o pipefail
+# helm lint hkube/
 CHART_VERSION=$(git describe --abbrev=0)
 VALUES_YAML_PATH=hkube/values.yaml NEW_VALUES_YAML_PATH=hkube/values.yaml ./version-updater
 APP_VERSION=$(grep systemversion hkube/values.yaml | awk -F': ' '{print $2}')
 mkdir -p /tmp/helm-charts
 helm dependency update hkube
-helm package --app-version=${APP_VERSION} --version=${APP_VERSION} -d /tmp/helm-charts hkube
+helm package --app-version=${APP_VERSION} --version=${CHART_VERSION} -d /tmp/helm-charts hkube
 git stash
-git checkout --track origin/gh-pages
-cp /tmp/helm-charts/hkube-${APP_VERSION}.tgz ./dev
+git remote -v
+git fetch
+git checkout -b gh-pages origin/gh-pages
+cp /tmp/helm-charts/hkube-${CHART_VERSION}.tgz ./dev
 helm repo index --merge ./dev/index.yaml /tmp/helm-charts/
 cp /tmp/helm-charts/index.yaml ./dev
 git add .
 git commit -m "update charts"
 git push
-git checkout master
+git checkout $BRANCH
 git stash apply
+echo git add hkube/values.yaml
 git add hkube/values.yaml
-git commit -m "update version to ${APP_VERSION} [skip ci]"
+echo git commit -m "update version to ${CHART_VERSION} [skip ci]"
+git commit -m "update version to ${CHART_VERSION} [skip ci]"
 git pull --no-commit
-git commit -m "merge from master [skip ci]"
+git commit -m "merge from $BRANCH [skip ci]"
 git push
